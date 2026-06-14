d581d7a2228008cb7edf947349613583c5a81eef
AmanKumarIT <akpailwar120@gmail.com>
2026-06-14 17:00:50 +0530

    .

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,6 +5,9 @@
 
 import axios from 'axios'
 
+console.log("API FILE LOADED");
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: '${import.meta.env.VITE_API_URL}/api/v1',
   timeout: 15000,
